4bd82923e0fd7f15ac41846d8d50878452620f67
Vlad Savenco (savencov) <savencov@coventry.ac.uk>
2020-10-05 18:36:37 +0100

    uwu walls

---
+++ LudumDare47Project/Assets/Scripts/SpawnWall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWall : MonoBehaviour
+{
+    public GameObject[] wall;
+
+    public GameObject Cluster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = Random.Range(0, wall.Length);
+
+        GameObject wally = Instantiate(wall[i], transform.position, Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0)));
+
+        wally.transform.parent = Cluster.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
